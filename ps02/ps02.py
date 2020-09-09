b2c043923a9238c683d4282615be8c012110e8b7
Bryan Ngo <bryanngo@berkeley.edu>
2020-09-08 23:15:25 -0700

    finished 2f

---
+++ ps02/ps02.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as clr
+from matplotlib.patches import Ellipse
+from math import sqrt
+
+smajor_axes = [
+    4.74e-3,
+    6.74e-3,
+    1.55e-2,
+    2.60e-2,
+    4.58e-2,
+    0.18
+]
+
+eccentricities = [
+    0.045,
+    0.026,
+    0.004,
+    0.012,
+    0.013,
+    0.015
+]
+
+sminor_axes = [sqrt(i**2 * (1 - j**2)) for i, j in zip(smajor_axes, eccentricities)]
+
+colors = [
+    '#0000ff',
+    '#00ff00',
+    '#00ffff',
+    '#ff0000',
+    '#ff00ff',
+    '#000000'
+]
+
+plt.title("Orbits of Kepler-11")
+ax = plt.subplot()
+for i, j, k in zip(smajor_axes, sminor_axes, colors):
+    ax.add_artist(Ellipse((0, 0), 2*i, 2*j, color=k,fill=False))
+plt.xlim((-0.2, 0.2))
+plt.ylim((-0.2, 0.2))
+plt.xlabel("Semimajor axis [au]")
+plt.ylabel("Semiminor axis [au]")
+plt.show()
+
+print(sminor_axes)
