2b0aa68344ad3641f19ff064998da81c8932c198
Bryan Ngo <bryanngo@berkeley.edu>
2020-09-20 01:43:25 -0700

    finished problems 1-3, working on 4

---
+++ ps03/ps03.py
@@ -0,0 +1,36 @@
+import numpy as np
+import csv
+
+wl_bins = np.linspace(1000, 10000, 11)
+num_bins = 10
+h, c = 6.62607015e-27, 299782458e+2
+f_bins = np.linspace(c / wl_bins[0], c / wl_bins[-1], 11)
+
+with open('ps03/3a.csv', 'w') as d1, open('ps03/3b.csv', 'w') as d2:
+    fields = ['wl', 'f']
+    writer1, writer2 = csv.DictWriter(d1, fields), csv.DictWriter(d2, fields)
+    writer1.writeheader()
+    writer2.writeheader()
+    for i in range(num_bins):
+        f_wl = 90 / (20000 * 50)
+        writer1.writerow({
+            fields[0]: wl_bins[i],
+            fields[1]: f_wl
+        })
+        div = f_bins[i] - f_bins[i + 1]
+        f_freq = (h * div) / (20000 * 50)
+        writer2.writerow({
+            fields[0]: f_bins[i],
+            fields[1]: f_freq
+        })
+    writer1.writerow({
+        fields[0]: wl_bins[-1],
+        fields[1]: f_wl
+    })
+    div = f_bins[-2] - f_bins[-1]
+    f_freq = (h * div) / (20000 * 50)
+    writer2.writerow({
+        fields[0]: f_bins[-1],
+        fields[1]: f_freq
+    })
+
