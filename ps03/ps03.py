78f82b9c79beb7ac099df6609abc5cc9773020c1
Bryan Ngo <bryanngo@berkeley.edu>
2020-09-21 23:06:55 -0700

    finished???

--- ps03/ps03.py
+++ ps03/ps03.py
@@ -1,38 +1,41 @@
 import numpy as np
 import csv
 
-wl_bins = np.linspace(1000, 10000, 11)
 num_bins = 10
-h, c = 6.62607015e-27, 299782458
-f_bins = np.linspace(c / (wl_bins[0] * 10e-10), c / (wl_bins[-1] * 10e-10), 11)
-print(f_bins)
+c = 29978245800
+wl_bins = 1e-8 * np.linspace(1000, 10000, 11)
+delta_wl = wl_bins[1] - wl_bins[0]
+f_bins = np.linspace(c / wl_bins[-1], c / wl_bins[0], 11)
+delta_f = f_bins[1] - f_bins[0]
+f_energy = []
 
-with open('ps03/3a.csv', 'w') as d1, open('ps03/3b.csv', 'w') as d2:
+with open('ps03/4a.csv', 'w') as d1, open('ps03/4b.csv', 'w') as d2:
     fields = ['wl', 'f']
     writer1, writer2 = csv.DictWriter(d1, fields), csv.DictWriter(d2, fields)
     writer1.writeheader()
     writer2.writeheader()
+    f_wl = 90 / (20000 * 50 * delta_wl)
     for i in range(num_bins):
-        f_wl = 90 / (20000 * 50)
         writer1.writerow({
             fields[0]: wl_bins[i],
             fields[1]: f_wl
         })
-        div = f_bins[i] - f_bins[i + 1]
-        f_freq = (h * div) / (20000 * 50)
-        print(f_freq)
+        f_divs = f_bins[0] + i * delta_f
+        f_freq = f_wl * (delta_wl / f_divs)
         writer2.writerow({
             fields[0]: f_bins[i],
             fields[1]: f_freq
         })
+        f_energy.append(f_freq * delta_f)
     writer1.writerow({
         fields[0]: wl_bins[-1],
         fields[1]: f_wl
     })
-    div = f_bins[-2] - f_bins[-1]
-    f_freq = (h * div) / (20000 * 50)
+    f_divs = f_bins[0] + (num_bins) * delta_f
+    f_freq = f_wl * (delta_wl / f_divs)
     writer2.writerow({
         fields[0]: f_bins[-1],
         fields[1]: f_freq
     })
-
+    f_energy.append(f_freq * delta_f)
+    print(20000 * 50 * sum(f_energy))
